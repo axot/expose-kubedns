7626024de3aa4bf569748ef33ca7954c7d99e290
Zheng SHAO <z-shao@colopl.co.jp>
2019-08-22 19:01:02 +0900

    add missing namespace

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ DEFAULT_COREDNS_IMAGE=coredns/coredns:1.6.2
 [ -z ${COREDNS_IMAGE} ] && COREDNS_IMAGE=${DEFAULT_COREDNS_IMAGE}
 
 cat codedns.yaml | sed "s@__COREDNS_IMAGE__@${COREDNS_IMAGE}@" | kubectl apply -f -
-echo kubectl scale deployment kube-dns-autoscaler --replicas 0
-echo kubectl scale deployment kube-dns --replicas 0
+
+echo kubectl scale deployment kube-dns-autoscaler --replicas 0 -n kube-system
+echo kubectl scale deployment kube-dns --replicas 0 -n kube-system
