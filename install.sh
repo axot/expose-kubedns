afeaf097ea7af0c90e329a625e24122422b7c628
Zheng SHAO <z-shao@colopl.co.jp>
2019-08-22 18:54:52 +0900

    use custom image

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
-kubectl apply -f codedns.yaml
+DEFAULT_COREDNS_IMAGE=coredns/coredns:1.6.2
+
+[ -z ${COREDNS_IMAGE} ] && COREDNS_IMAGE=${DEFAULT_COREDNS_IMAGE}
+
+cat codedns.yaml | sed "s@__COREDNS_IMAGE__@${COREDNS_IMAGE}@" | kubectl apply -f -
 echo kubectl scale deployment kube-dns-autoscaler --replicas 0
 echo kubectl scale deployment kube-dns --replicas 0
